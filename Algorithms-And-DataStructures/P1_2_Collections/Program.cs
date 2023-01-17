d2adb13a8e941c4b0a66150b9df487b724d3acaf
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-17 07:11:34 +0100

    Add ArrayList Forsbergs

--- Algorithms-And-DataStructures/P1_2_Collections/Program.cs
+++ Algorithms-And-DataStructures/P1_2_Collections/Program.cs
@@ -11,9 +11,25 @@ Numbers.Add(137);
 Numbers.Add(1000);
 Numbers.Add(-200);
 
-IEnumerator enumerator = Numbers.GetEnumerator();
-foreach (int item in Numbers)
+ArrayList Forsbergs = new ArrayList();
+Forsbergs.Add(true);
+Forsbergs.Add("Forsbergs");
+Forsbergs.Add('a');
+Forsbergs.Add(1000);
+Forsbergs.Add(0.12f);
+
+foreach (var item in Numbers)
 {
     Console.WriteLine(item);
 }
 
+IEnumerator enumerator = Forsbergs.GetEnumerator();
+foreach (var item in Forsbergs)
+{
+    Console.WriteLine(item);
+}
+
+for (int i = 0; i < Forsbergs.Count; i++)
+{
+    Console.WriteLine(Forsbergs[i]);
+}
