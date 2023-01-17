566ed40423139902dd298a5811991f62a2fbe695
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-17 06:52:53 +0100

    Update Program.cs
    Was running Hello World program, not P1_2... That's why was printing Hello World :D

--- Algorithms-And-DataStructures/P1_2_Collections/Program.cs
+++ Algorithms-And-DataStructures/P1_2_Collections/Program.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections;
+﻿//Create a Project named P1_2_Collections
+//Create a List to store the numbers 137,1000, -200
+//Use a for-Loop and the index-Operator [] to print all values in the List
+//Create an ArrayList to store the values true, "Forsbergs", 'a', 1000, .12f;
+//Use a for-Loop and the index-Operator [] to print all values in the ArrayList
 
-List<int> List = new List<int>();
-List.Add(137);
-List.Add(1000);
-List.Add(-200);
-var list = List.ToArray();
+using System.Collections;
 
-IEnumerator enumerator = List.GetEnumerator();
-foreach (var item in List)
+List<int> Numbers = new List<int>();
+Numbers.Add(137);
+Numbers.Add(1000);
+Numbers.Add(-200);
+
+IEnumerator enumerator = Numbers.GetEnumerator();
+foreach (int item in Numbers)
 {
     Console.WriteLine(item);
 }
+
