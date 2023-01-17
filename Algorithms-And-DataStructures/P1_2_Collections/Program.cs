d4076f1d9930829df6968f8425cb4f76ff727243
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-17 06:40:32 +0100

    create List
    but prints Hello World ???

--- Algorithms-And-DataStructures/P1_2_Collections/Program.cs
+++ Algorithms-And-DataStructures/P1_2_Collections/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections;
 
-Console.WriteLine("Hello, World P1_2_Collections");
+List<int> List = new List<int>();
+List.Add(137);
+List.Add(1000);
+List.Add(-200);
+var list = List.ToArray();
+
+IEnumerator enumerator = List.GetEnumerator();
+foreach (var item in List)
+{
+    Console.WriteLine(item);
+}
