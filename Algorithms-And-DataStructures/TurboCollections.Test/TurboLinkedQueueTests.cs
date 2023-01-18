6143ac8351b2b6a2ed4229c7c406d71611082e1f
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-18 14:32:35 +0100

    Create TurboLinkQueue & Test
    create enqueue and  test

---
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboLinkedQueueTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TurboCollections.Test;
+
+public class TurboLinkedQueueTests
+{
+    [Test]
+    public void CountTest()
+    {
+
+    }
+
+    [Test]
+    public void EnqueueTest()
+    {
+        var stack = new TurboLinkedStack<int>();
+                
+        stack.Push(1);
+        stack.Push(5);
+        stack.Push(13);
+                
+        CollectionAssert.AreEqual(stack, new[]{13, 5, 1});
+    }
+}
---
+++ Algorithms-And-DataStructures/TurboCollections/TurboLinkedQueue.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace TurboCollections;
+
+public interface ITurboQueue<T> : IEnumerable<T>
+    {
+        public class Node
+        {
+            public T Value;
+            public Node Previous;
+        }
+
+        // returns the current amount of items contained in the stack.
+        //int count;
+        // adds one item to the back of the queue.
+        public void Enqueue(T item)
+        {
+            Node newNode = new Node { Value = item, Previous = FirstNode };
+            FirstNode = newNode;
+        }
+
+        Node FirstNode { get; set; }
+
+        // returns the item in the front of the queue without removing it.
+        T Peek();
+        // returns the item in the front of the queue and removes it at the same time.
+        T Dequeue();
+        // removes all items from the queue.
+        void Clear();
+    }
