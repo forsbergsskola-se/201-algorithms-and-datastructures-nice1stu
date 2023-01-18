bb6df6dbf2c0e38922381d2b70c0f83669866c9f
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-18 04:44:38 +0100

    create Push and Unit test
    Thank God I got it to finally work

--- Algorithms-And-DataStructures/TurboCollections.Test/TurboLinkedStackTests.cs
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboLinkedStackTests.cs
@@ -2,5 +2,20 @@
 
 public class TurboLinkedStackTests
 {
-    
+        [Test]
+        public void PushTest()
+        {
+                // Arrange
+                var stack = new TurboLinkedStack<int>();
+
+                // Act
+                stack.Push(1);
+                stack.Push(2);
+                stack.Push(3);
+
+                // Assert
+                Assert.AreEqual(3, stack.LastNode.Value);
+                Assert.AreEqual(2, stack.LastNode.Previous.Value);
+                Assert.AreEqual(1, stack.LastNode.Previous.Previous.Value);
+        }
 }
