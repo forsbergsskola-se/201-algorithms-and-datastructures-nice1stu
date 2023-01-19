50d64507677af0af4883ba2379bfc3ae9add229e
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-19 02:39:40 +0100

    tidy up namespace

--- Algorithms-And-DataStructures/SpotifySongQueue/Program.cs
+++ Algorithms-And-DataStructures/SpotifySongQueue/Program.cs
@@ -5,10 +5,6 @@
 //[s]kip to next song: If there is another song in the queue, it prints $"Now Playing {songName}.". else, it says "No more songs in the Queue. Add new Songs to the Queue first."
 //[a]dd new song to the queue: The application asks for a Song Name and then adds it to the Queue.
 
-using System.Collections;
-
-namespace TurboCollections;
-
 public class SpotifySongQueue
 {
     static void Main()
@@ -19,14 +15,15 @@ public class SpotifySongQueue
         {
             Console.WriteLine("What would you like to do? [s]kip or [a]dd?");
             string userInput = Console.ReadLine();
+            string songName = null;
             if (userInput == "s")
             {
-                
+                if (songName != null) Console.WriteLine($"Now Playing: {songName}");
             }
             else if (userInput == "a")
             {
                 Console.WriteLine("Enter the Song's Name");
-                string songName = Console.ReadLine();
+                songName = Console.ReadLine();
                 queue.Enqueue(songName);
             }
         }
