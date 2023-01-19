8977c02f9a700a47752bf44e89f8079147997cb8
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-19 02:20:33 +0100

    Reset... not suppose to use test

--- Algorithms-And-DataStructures/SpotifySongQueue/Program.cs
+++ Algorithms-And-DataStructures/SpotifySongQueue/Program.cs
@@ -6,82 +6,10 @@
 //[a]dd new song to the queue: The application asks for a Song Name and then adds it to the Queue.
 
 using System.Collections;
-using NUnit.Framework;
 
-namespace TurboCollections.Test;
+namespace TurboCollections;
+
 public class SpotifySongQueue
 {
-    [Test]
-    public void EnqueueTestcheckStoredValues()
-    {
-        var queue = new TurboLinkedQueue<int>();
-                
-        queue.Enqueue(100);
-        queue.Enqueue(5);
-        queue.Enqueue(13);
-        queue.Enqueue(101);
-        queue.Enqueue(54);
-                
-        CollectionAssert.AreEqual(queue, new[]{100, 5, 13, 101, 54});
-    }
-    
-    [Test]
-    public void PeekTestReturn1stValue()
-    {
-        var queue = new TurboLinkedQueue<int>();
-        queue.Enqueue(100);
-        queue.Enqueue(5);
-        queue.Enqueue(13);
-        queue.Enqueue(101);
-        queue.Enqueue(54);
 
-        var result = queue.Peek();
-
-        Assert.AreEqual(100, result);
-        Assert.AreEqual(100, result);
-    }
-    
-    [Test]
-    public void DequeueTest()
-    {
-        var queue = new TurboLinkedQueue<int>();
-        queue.Enqueue(100);
-        queue.Enqueue(5);
-        queue.Enqueue(13);
-        queue.Enqueue(101);
-        queue.Enqueue(54);
-
-        queue.Dequeue();
-        
-        Assert.AreEqual(5, queue.Peek());
-    }
-
-    [Test]
-
-    public void ClearTest()
-    {
-        var queue = new TurboLinkedQueue<int>();
-        queue.Enqueue(100);
-        queue.Enqueue(5);
-        queue.Enqueue(13);
-        queue.Enqueue(101);
-        queue.Enqueue(54);
-        
-        queue.Clear();
-        
-        Assert.AreEqual(0, queue.Count);
-    }
-
-    [Test]
-    public void CountTest()
-    {
-        var queue = new TurboLinkedQueue<int>();
-        queue.Enqueue(100);
-        queue.Enqueue(5);
-        queue.Enqueue(13);
-        queue.Enqueue(101);
-        queue.Enqueue(54);
-        
-        Assert.AreEqual(5, queue.Count);
-    }
 }
