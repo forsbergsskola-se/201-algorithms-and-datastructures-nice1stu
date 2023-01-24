3c44a26a66a05e83a483deebebc5333583035ca7
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-24 03:48:49 +0100

    Update TurboFibonacciTests.cs
    Include stopwatch function from systems.diagnostics

--- Algorithms-And-DataStructures/TurboCollections.Test/TurboFibonacciTests.cs
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboFibonacciTests.cs
@@ -1,19 +1,26 @@
-﻿using NUnit.Framework;
+﻿using System.Diagnostics;
 
-namespace TurboCollections
+namespace TurboCollections.Test;
+
+public class TurboFibonacciTests
 {
-    [TestFixture]
-    public class TurboFibonacciTests
+    [Test]
+    public void TestFibonacciRecursive()
     {
-        [Test]
-        public void TestFibonacciRecursive()
-        {
-            Assert.That(TurboFibonacci.FibonacciRecursive(0), Is.EqualTo(0));
-            Assert.That(TurboFibonacci.FibonacciRecursive(1), Is.EqualTo(1));
-            Assert.That(TurboFibonacci.FibonacciRecursive(2), Is.EqualTo(1));
-            Assert.That(TurboFibonacci.FibonacciRecursive(3), Is.EqualTo(2));
-            Assert.That(TurboFibonacci.FibonacciRecursive(4), Is.EqualTo(3));
-            Assert.That(TurboFibonacci.FibonacciRecursive(40), Is.EqualTo(102334155));
-        }
+        Stopwatch stopwatch = new Stopwatch();
+        int n = 40;
+
+        stopwatch.Start();
+        int result1 = TurboFibonacci.FibonacciRecursive(n);
+        stopwatch.Stop();
+        Console.WriteLine("Recursive: " + result1);
+        Console.WriteLine("Time taken: " + stopwatch.ElapsedMilliseconds + "ms");
+            
+        Assert.That(TurboFibonacci.FibonacciRecursive(0), Is.EqualTo(0));
+        Assert.That(TurboFibonacci.FibonacciRecursive(1), Is.EqualTo(1));
+        Assert.That(TurboFibonacci.FibonacciRecursive(2), Is.EqualTo(1));
+        Assert.That(TurboFibonacci.FibonacciRecursive(3), Is.EqualTo(2));
+        Assert.That(TurboFibonacci.FibonacciRecursive(4), Is.EqualTo(3));
+        Assert.That(TurboFibonacci.FibonacciRecursive(40), Is.EqualTo(102334155));
     }
 }
