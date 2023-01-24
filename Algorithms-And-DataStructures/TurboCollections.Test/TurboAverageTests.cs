4c3d030d0900a4528a42ee3ab49f851b04620076
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-24 03:23:20 +0100

    Create TurboAverageTests.cs

---
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboAverageTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using TurboCollections;
+
+[TestFixture]
+public class TurboAverageTests
+{
+    [Test]
+    public void TestCalculateAverage()
+    {
+        Assert.That(TurboAverage.CalculateAverage(new double[] { 1, 2, 3 }), Is.EqualTo(2));
+        Assert.That(TurboAverage.CalculateAverage(new double[] { 69, 13, 666, 7 }), Is.EqualTo(188.75));
+        Assert.That(TurboAverage.CalculateAverage(new double[] { -1, 0, 1 }), Is.EqualTo(0));
+        Assert.That(TurboAverage.CalculateAverage(new double[] { 1, 1, 1, 1 }), Is.EqualTo(1));
+        Assert.That(TurboAverage.CalculateAverage(new double[] { 2.5, 3.5, 4.5 }), Is.EqualTo(3.5));
+    }
+}
