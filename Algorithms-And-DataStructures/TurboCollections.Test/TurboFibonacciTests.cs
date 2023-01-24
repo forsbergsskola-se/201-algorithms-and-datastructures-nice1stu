c75e9f1df70080786986bb330596cbe3a1d518ec
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-24 03:40:42 +0100

    create turbofibonaccitests
    test recursive to 40

---
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboFibonacciTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace TurboCollections
+{
+    [TestFixture]
+    public class TurboFibonacciTests
+    {
+        [Test]
+        public void TestFibonacciRecursive()
+        {
+            Assert.That(TurboFibonacci.FibonacciRecursive(0), Is.EqualTo(0));
+            Assert.That(TurboFibonacci.FibonacciRecursive(1), Is.EqualTo(1));
+            Assert.That(TurboFibonacci.FibonacciRecursive(2), Is.EqualTo(1));
+            Assert.That(TurboFibonacci.FibonacciRecursive(3), Is.EqualTo(2));
+            Assert.That(TurboFibonacci.FibonacciRecursive(4), Is.EqualTo(3));
+            Assert.That(TurboFibonacci.FibonacciRecursive(40), Is.EqualTo(102334155));
+        }
+    }
+}
--- Algorithms-And-DataStructures/TurboCollections/TurboFibonacci.cs
+++ Algorithms-And-DataStructures/TurboCollections/TurboFibonacci.cs
@@ -1,6 +1,20 @@
 ﻿namespace TurboCollections;
 
+//the Fibonacci sequence, in which each number is the sum of the two preceding ones.
+
 public class TurboFibonacci
 {
-    
+    //Recursive Fn = Fn−1+Fn−2
+    public static int FibonacciRecursive(int nthNumber) 
+    {
+        if (nthNumber == 0)
+        {
+            return 0;
+        }
+        else if (nthNumber == 1)
+        {
+            return 1;
+        }
+        return FibonacciRecursive(nthNumber-1) + FibonacciRecursive(nthNumber-2);
+    }
 }
