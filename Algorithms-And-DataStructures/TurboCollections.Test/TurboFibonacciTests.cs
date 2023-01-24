382d68369dc64126a00969d89b541d168a1d31f0
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-24 04:02:28 +0100

    Update TurboFibonacciTests.cs
    create unit test and timer for iterative

--- Algorithms-And-DataStructures/TurboCollections.Test/TurboFibonacciTests.cs
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboFibonacciTests.cs
@@ -23,4 +23,24 @@ public class TurboFibonacciTests
         Assert.That(TurboFibonacci.FibonacciRecursive(4), Is.EqualTo(3));
         Assert.That(TurboFibonacci.FibonacciRecursive(40), Is.EqualTo(102334155));
     }
+    
+    [Test]
+    public void TestFibonacciIterative()
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        int n = 40;
+
+        stopwatch.Start();
+        int result1 = TurboFibonacci.FibonacciIterative(n);
+        stopwatch.Stop();
+        Console.WriteLine("Iterative: " + result1);
+        Console.WriteLine("Time taken: " + stopwatch.ElapsedMilliseconds + "ms");
+            
+        Assert.That(TurboFibonacci.FibonacciRecursive(0), Is.EqualTo(0));
+        Assert.That(TurboFibonacci.FibonacciRecursive(1), Is.EqualTo(1));
+        Assert.That(TurboFibonacci.FibonacciRecursive(2), Is.EqualTo(1));
+        Assert.That(TurboFibonacci.FibonacciRecursive(3), Is.EqualTo(2));
+        Assert.That(TurboFibonacci.FibonacciRecursive(4), Is.EqualTo(3));
+        Assert.That(TurboFibonacci.FibonacciRecursive(40), Is.EqualTo(102334155));
+    }
 }
