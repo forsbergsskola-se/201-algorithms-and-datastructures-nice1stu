77467804ea856c05185ff230f99e9171c6e65822
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-24 04:42:24 +0100

    Update Turbo Sort to include Bubble Sort

--- Algorithms-And-DataStructures/TurboCollections.Test/TurboSortTests.cs
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboSortTests.cs
@@ -1,6 +1,25 @@
-﻿namespace TurboCollections.Test;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework;
 
-public class TurboSortTests
+namespace TurboCollections.Tests
 {
-    
+    public class TurboSortTests
+    {
+        private List<int> _defaultList = new List<int> { 3, 1, 2, 4, 5 };
+        private List<int> _expectedSortedList = new List<int> { 1, 2, 3, 4, 5 };
+
+        [Test]
+        public void SelectionSort_SortsList()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var actual = TurboSort.SelectionSort(_defaultList);
+            stopwatch.Stop();
+            var elapsed = stopwatch.Elapsed;
+            
+            CollectionAssert.AreEqual(_expectedSortedList, actual);
+            Console.WriteLine($"Elapsed time: {elapsed}");
+        }
+    }
 }
--- Algorithms-And-DataStructures/TurboCollections/TurboSort.cs
+++ Algorithms-And-DataStructures/TurboCollections/TurboSort.cs
@@ -24,5 +24,25 @@ namespace TurboCollections
             }
             return input;
         }
+        
+        //Bubble Sort - a comparison-based algorithm in which each pair of adjacent elements is compared and the elements are swapped if they are not in order.
+        public static List<int> BubbleSort(List<int> input)
+        {
+            int temp;
+            for (int i = 0; i < input.Count - 1; i++)
+            {
+                for (int j = 0; j < input.Count - i - 1; j++)
+                {
+                    if (input[j] > input[j + 1])
+                    {
+                        temp = input[j];
+                        input[j] = input[j + 1];
+                        input[j + 1] = temp;
+                    }
+                }
+            }
+            return input;
+        }
     }
 }
+
