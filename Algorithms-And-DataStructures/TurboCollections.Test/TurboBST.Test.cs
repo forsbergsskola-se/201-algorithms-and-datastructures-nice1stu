7a27440911be9de49bb75303b8c7b69276076b7e
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-31 21:50:46 +0100

    Add search for existing node test

--- Algorithms-And-DataStructures/TurboCollections.Test/TurboBST.Test.cs
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboBST.Test.cs
@@ -41,5 +41,22 @@ namespace TurboCollections.Tests
             Assert.That(tree.Search(40), Is.True);
             Assert.That(tree.Search(50), Is.True);
         }
+        
+        [Test]
+        public void Search_ReturnsTrueForExistingNode()
+        {
+            TurboBST tree = new TurboBST();
+            tree.Insert(10);
+            tree.Insert(20);
+            tree.Insert(30);
+            tree.Insert(40);
+            tree.Insert(50);
+            
+            Assert.That(tree.Search(10), Is.True);
+            Assert.That(tree.Search(30), Is.True);
+            Assert.That(tree.Search(50), Is.True);
+            Assert.That(tree.Search(60), Is.False);
+            
+        }
     }
 }
