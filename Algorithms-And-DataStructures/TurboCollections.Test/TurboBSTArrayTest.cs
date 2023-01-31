5585dfefb516c857d3de6b261b94774f6e49bcb4
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-31 22:46:43 +0100

    Tried doing TurboBSTArray & Tests ... not working

---
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboBSTArrayTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace TurboCollections.Test;
+
+[TestFixture]
+public class TurboBSTArrayTest
+{/*
+    public class TurboBSTArrayTests
+    {
+        [Test]
+        public void Insert_ShouldAddValueToBSTArray()
+        {
+            TurboBSTArray _bstArray = new TurboBSTArray();
+            _bstArray.Insert(5);
+            _bstArray.Insert(3);
+            _bstArray.Insert(7);
+            _bstArray.Insert(2);
+            _bstArray.Insert(4);
+            _bstArray.Insert(6);
+            _bstArray.Insert(8);
+
+            CollectionAssert.AreEqual(new int[] { 5, 3, 2, 4, 7, 6, 8 }, _bstArray._tree.Take(_bstArray._size));
+        }
+    }*/
+}
---
+++ Algorithms-And-DataStructures/TurboCollections/TurboBSTArray.cs
@@ -0,0 +1,78 @@
+﻿namespace TurboCollections;
+
+public class TurboBSTArray
+{
+    /*public int[] _tree;
+    public int _size;
+    private const int MaxSize = 10;
+
+    public TurboBSTArray()
+    {
+        _tree = new int[MaxSize];
+        _size = 0;
+    }
+
+    public bool Search(int value)
+    {
+        int index = 0;
+        while (index < _size)
+        {
+            if (_tree[index] == value) return true;
+            if (_tree[index] > value) index = 2 * index + 1;
+            else index = 2 * index + 2;
+        }
+
+        return false;
+    }
+
+    public void Insert(int key)
+    {
+        int index = 0;
+        while (index < _size)
+        {
+            if (_tree[index] > key) index = 2 * index + 1;
+            else index = 2 * index + 2;
+        }
+        _tree[index] = key;
+        _size++;
+    }
+
+    public void Inorder()
+    {
+        InorderRec(0);
+    }
+
+    private void InorderRec(int index)
+    {
+        if (index >= _size) return;
+
+        InorderRec(2 * index + 1);
+        Console.Write(_tree[index] + " ");
+        InorderRec(2 * index + 2);
+    }
+
+    public void Delete(int key)
+    {
+        int index = 0;
+        while (index < _size)
+        {
+            if (_tree[index] == key) break;
+            if (_tree[index] > key) index = 2 * index + 1;
+            else index = 2 * index + 2;
+        }
+        if (index >= _size) return;
+
+        int leftmost = 2 * index + 2;
+        while (leftmost < _size)
+        {
+            int right = leftmost + 1;
+            if (right < _size && _tree[right] < _tree[leftmost]) leftmost = right;
+            _tree[index] = _tree[leftmost];
+            index = leftmost;
+            leftmost = 2 * leftmost + 2;
+        }
+
+        _tree[index] = _tree[_size - 1];
+        _size--;
+    }*/
+}
