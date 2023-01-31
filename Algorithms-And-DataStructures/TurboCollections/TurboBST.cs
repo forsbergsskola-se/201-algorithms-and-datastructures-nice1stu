229ed73a0825c96baf6cba41c2d78b22776774b4
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-31 21:17:49 +0100

    Add Search, Delete methods

--- Algorithms-And-DataStructures/TurboCollections/TurboBST.cs
+++ Algorithms-And-DataStructures/TurboCollections/TurboBST.cs
@@ -18,6 +18,20 @@ class BinarySearchTree
     private Node? _root;
     
     BinarySearchTree() { _root = null; }
+    
+    public bool Search(int value)
+    {
+        return SearchRec(_root, value);
+    }
+
+    private bool SearchRec(Node? root, int value)
+    {
+        if (root == null) return false;
+        if (root.Key == value) return true;
+
+        if (value < root.Key) return SearchRec(root.Left, value);
+        return SearchRec(root.Right, value);
+    }
 
     void Insert(int key) { _root = InsertRec(_root, key); }
     
@@ -40,7 +54,52 @@ class BinarySearchTree
         Console.Write(root.Key + " ");
         InorderRec(root.Right);
     }
- 
+    
+    void Delete(int key)
+    {
+        _root = DeleteRec(_root, key);
+    }
+
+    Node? DeleteRec(Node? root, int key)
+    {
+        switch (root)
+        {
+            case null:
+                return null;
+        }
+
+        if (key >= root.Key)
+        {
+            if (key <= root.Key)
+            {
+                if (root.Left == null) return root.Right;
+
+                if (root.Right == null) return root.Left;
+
+                var min = MinValue(root.Right);
+                root.Key = min;
+                root.Right = DeleteRec(root.Right, min);
+            }
+            else
+                root.Right = DeleteRec(root.Right, key);
+        }
+        else
+            root.Left = DeleteRec(root.Left, key);
+
+        return root;
+    }
+
+    int MinValue(Node? root)
+    {
+        int min = root.Key;
+        while (root.Left != null)
+        {
+            min = root.Left.Key;
+            root = root.Left;
+        }
+        return min;
+    }
+
 
     public static void Main(String[] args)
     {
@@ -52,15 +111,15 @@ class BinarySearchTree
           30      70
          /  \    /  \
        20   40  60   80 */
-        tree.Insert(50);
+       /* tree.Insert(50);
         tree.Insert(30);
         tree.Insert(20);
         tree.Insert(40);
         tree.Insert(70);
         tree.Insert(60);
-        tree.Insert(80);
+        tree.Insert(80);*/
  
         // Print inorder traversal of the BST
-        tree.Inorder();
+        //tree.Inorder();
     }
 }
