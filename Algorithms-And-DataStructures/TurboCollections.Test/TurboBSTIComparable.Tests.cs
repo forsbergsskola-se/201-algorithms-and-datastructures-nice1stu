878a80d4cedd35340d4cd27b490330ebb633af20
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-02-01 21:13:43 +0100

    Add Remove method & test

--- Algorithms-And-DataStructures/TurboCollections.Test/TurboBSTIComparable.Tests.cs
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboBSTIComparable.Tests.cs
@@ -25,7 +25,7 @@ public class TurboBSTIComparable_Tests
             Assert.That(result, Is.EqualTo(i == 2 || i == 3 || i == 4 || i == 5 || i == 7 || i == 9));
         }
     }
-    
+
     [Test]
 
     public void SearchWhenValueisNotPresentReturnFalse()
@@ -36,8 +36,53 @@ public class TurboBSTIComparable_Tests
         tree._root.Left = new TurboBSTIComparable<int>.Node(3);
         tree._root.Right = new TurboBSTIComparable<int>.Node(7);
 
-        var result = tree.Search(6);
-        
-        Assert.That(result, Is.False);
+        for (int i = 1; i <= 9; i++)
+        {
+            var result = tree.Search(i);
+            Console.WriteLine("Searching for " + i + ": " + result);
+            Assert.That(result, Is.EqualTo(i == 3 || i == 5 || i == 7));
+        }
     }
+    
+    [Test]
+    public void InsertMethodShouldInsertValueIntoTheTree()
+    {
+        var tree = new TurboBSTIComparable<int>();
+
+        tree.Insert(5);
+        tree.Insert(3);
+        tree.Insert(7);
+        tree.Insert(2);
+        tree.Insert(4);
+        tree.Insert(9);
+
+        for (int i = 1; i <= 9; i++)
+        {
+                var result = tree.Search(i);
+                Console.WriteLine("Searching for " + i + ": " + result);
+                Assert.That(result, Is.EqualTo(i == 2 || i == 3 || i == 4 || i == 5 || i == 7 || i == 9));
+        }
+    }
+    
+    [Test]
+    public void RemoveMethodRemovesNodeWithGivenValue()
+    {
+        var tree = new TurboBSTIComparable<int>();
+
+        tree._root = new TurboBSTIComparable<int>.Node(5);
+        tree._root.Left = new TurboBSTIComparable<int>.Node(3);
+        tree._root.Right = new TurboBSTIComparable<int>.Node(7);
+        tree._root.Left.Left = new TurboBSTIComparable<int>.Node(2);
+        tree._root.Left.Right = new TurboBSTIComparable<int>.Node(4);
+        tree._root.Right.Right = new TurboBSTIComparable<int>.Node(9);
+
+        tree.Remove(3);
+        for (int i = 1; i <= 9; i++)
+        {
+            var result = tree.Search(i);
+            Console.WriteLine("Searching for " + i + ": " + result);
+            Assert.That(result, Is.EqualTo(i == 2 || i == 4 || i == 5 || i == 7 || i == 9));
+        }
+    }
+
 }
--- Algorithms-And-DataStructures/TurboCollections/TurboBSTIComparable.cs
+++ Algorithms-And-DataStructures/TurboCollections/TurboBSTIComparable.cs
@@ -35,4 +35,77 @@ public class TurboBSTIComparable<T> where T:IComparable<T>
         if (value.CompareTo(root.Key) < 0) return SearchRec(root.Left, value);
         return SearchRec(root.Right, value);
     }
+    
+    public void Insert(T value)
+    {
+        _root = InsertRec(_root, value);
+    }
+
+    private Node InsertRec(Node? root, T value)
+    {
+        if (root == null)
+        {
+            root = new Node(value);
+            return root;
+        }
+
+        if (value.CompareTo(root.Key) < 0)
+        {
+            root.Left = InsertRec(root.Left, value);
+        }
+        else if (value.CompareTo(root.Key) > 0)
+        {
+            root.Right = InsertRec(root.Right, value);
+        }
+
+        return root;
+    }
+
+    public void Remove(T value)
+    {
+        _root = RemoveRec(_root, value);
+    }
+
+    private Node? RemoveRec(Node? root, T value)
+    {
+        if (root == null) return root;
+
+        if (value.CompareTo(root.Key) < 0)
+        {
+            root.Left = RemoveRec(root.Left, value);
+            return root;
+        }
+
+        if (value.CompareTo(root.Key) > 0)
+        {
+            root.Right = RemoveRec(root.Right, value);
+            return root;
+        }
+
+        if (root.Left == null)
+        {
+            return root.Right;
+        }
+        if (root.Right == null)
+        {
+            return root.Left;
+        }
+
+        T min = MinValue(root.Right);
+        root.Key = min;
+        root.Right = RemoveRec(root.Right, min);
+        return root;
+    }
+
+    private T MinValue(Node? root)
+    {
+        T minValue = root!.Key;
+        while (root!.Left != null)
+        {
+            minValue = root.Left.Key;
+            root = root.Left;
+        }
+        return minValue;
+    }
+
 }
