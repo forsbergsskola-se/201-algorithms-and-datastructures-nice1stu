8695114211fc32c132928156926c9530e3b07c75
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-02-01 20:40:28 +0100

    create for loop to test all number in tree

--- Algorithms-And-DataStructures/TurboCollections.Test/TurboBSTIComparable.Tests.cs
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboBSTIComparable.Tests.cs
@@ -5,5 +5,39 @@ namespace TurboCollections.Test;
 [TestFixture]
 public class TurboBSTIComparable_Tests
 {
+    [Test]
+
+    public void SearchWhenValueisPresentReturnTrue()
+    {
+        var tree = new TurboBSTIComparable<int>();
+
+        tree._root = new TurboBSTIComparable<int>.Node(5);
+        tree._root.Left = new TurboBSTIComparable<int>.Node(3);
+        tree._root.Right = new TurboBSTIComparable<int>.Node(7);
+        tree._root.Left.Left = new TurboBSTIComparable<int>.Node(2);
+        tree._root.Left.Right = new TurboBSTIComparable<int>.Node(4);
+        tree._root.Right.Right = new TurboBSTIComparable<int>.Node(9);
+
+        for (int i = 1; i <= 9; i++)
+        {
+            var result = tree.Search(i);
+            Console.WriteLine("Searching for " + i + ": " + result);
+            Assert.That(result, Is.EqualTo(i == 2 || i == 3 || i == 4 || i == 5 || i == 7 || i == 9));
+        }
+    }
     
+    [Test]
+
+    public void SearchWhenValueisNotPresentReturnFalse()
+    {
+        var tree = new TurboBSTIComparable<int>();
+
+        tree._root = new TurboBSTIComparable<int>.Node(5);
+        tree._root.Left = new TurboBSTIComparable<int>.Node(3);
+        tree._root.Right = new TurboBSTIComparable<int>.Node(7);
+
+        var result = tree.Search(6);
+        
+        Assert.That(result, Is.False);
+    }
 }
