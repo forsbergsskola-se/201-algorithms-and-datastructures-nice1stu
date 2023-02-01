1cf6c6188b5a5456ad5d8d70dedb26d123b2cf06
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-02-01 20:28:36 +0100

    create search

--- Algorithms-And-DataStructures/TurboCollections/TurboBSTIComparable.cs
+++ Algorithms-And-DataStructures/TurboCollections/TurboBSTIComparable.cs
@@ -4,6 +4,35 @@ public class TurboBSTIComparable<T> where T:IComparable<T>
 {
     public class Node
     {
-        
+        public T Key;
+        public Node? Left;
+        public Node? Right;
+
+        public Node(T item)
+        {
+            Key = item;
+            Left = Right = null;
+        }
+    }
+
+    public Node? _root;
+
+    public TurboBSTIComparable()
+    {
+        _root = null;
+    }
+
+    public bool Search(T value)
+    {
+        return SearchRec(_root, value);
+    }
+
+    private bool SearchRec(Node? root, T value)
+    {
+        if (root == null) return false;
+        if (root.Key.CompareTo(value) == 0) return true;
+
+        if (value.CompareTo(root.Key) < 0) return SearchRec(root.Left, value);
+        return SearchRec(root.Right, value);
     }
 }
