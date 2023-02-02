84dc46752f8d4d27ef42d05102c8ef6583aa8560
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-02-02 11:34:32 +0100

    Finish unit tests

--- Algorithms-And-DataStructures/TurboCollections.Test/TurboCloneBST.Test.cs
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboCloneBST.Test.cs
@@ -1,31 +1,69 @@
-﻿using NUnit.Framework;
+﻿namespace TurboCollections.Test;
 
 [TestFixture]
-public class TurboCloneBSTTests
+public class TurboCloneBstTests
 {
     [Test]
-    public void CloneTreeWithValidBSTShouldReturnClonedBST()
+    public void CloneTreeWithValidBstShouldReturnClonedBst()
     {
         BST bst = new BST();
-        bst.root = new Node(5);
-        bst.root.left = new Node(2);
-        bst.root.right = new Node(8);
+        bst.root = new global::Node(5);
+        bst.root.left = new global::Node(2);
+        bst.root.right = new global::Node(8);
 
-        BST clonedBST = new BST();
-        clonedBST.root = bst.CloneTree(bst.root);
+        BST clonedBst = new BST();
+        clonedBst.root = bst.CloneTree(bst.root);
 
-        Assert.AreEqual(bst.root.data, clonedBST.root.data);
-        Assert.AreEqual(bst.root.left.data, clonedBST.root.left.data);
-        Assert.AreEqual(bst.root.right.data, clonedBST.root.right.data);
+        Assert.AreEqual(bst.root.data, clonedBst.root.data);
+        Assert.AreEqual(bst.root.left.data, clonedBst.root.left.data);
+        Assert.AreEqual(bst.root.right.data, clonedBst.root.right.data);
     }
 
     [Test]
-    public void CloneTree_WithEmptyBST_ShouldReturnNull()
+    public void CloneTreeWithEmptyBST_ShouldReturnNull()
     {
         BST bst = new BST();
-        BST clonedBST = new BST();
-        clonedBST.root = bst.CloneTree(bst.root);
+        BST clonedBst = new BST();
+        clonedBst.root = bst.CloneTree(bst.root);
 
-        Assert.IsNull(clonedBST.root);
+        Assert.IsNull(clonedBst.root);
+    }
+    
+    [Test]
+    public void RemoveNode_ShouldRemoveNodeFromClonedTreeButNotFromOriginalTree()
+    {
+        BST originalBst = new BST();
+        originalBst.root = new global::Node(4);
+        originalBst.root.left = new global::Node(2);
+        originalBst.root.right = new global::Node(5);
+        originalBst.root.left.left = new global::Node(1);
+        originalBst.root.left.right = new global::Node(3);
+        originalBst.root.right.right = new global::Node(6);
+
+        BST clonedBst = new BST();
+        clonedBst.root = originalBst.CloneTree(originalBst.root);
+
+        clonedBst.RemoveNode(5);
+
+        Assert.IsTrue(NodeExists(originalBst.root, 5));
+        Assert.IsFalse(NodeExists(clonedBst.root, 5));
+        Assert.IsTrue(NodeExists(clonedBst.root, 1));
+        Assert.IsTrue(NodeExists(clonedBst.root, 2));
+        Assert.IsTrue(NodeExists(clonedBst.root, 3));
+        Assert.IsTrue(NodeExists(clonedBst.root, 4));
+        Assert.IsTrue(NodeExists(clonedBst.root, 6));
+    }
+
+    private bool NodeExists(global::Node node, int value)
+    {
+        if (node == null)
+        {
+            return false;
+        }
+        if (node.data == value)
+        {
+            return true;
+        }
+        return NodeExists(node.left, value) || NodeExists(node.right, value);
     }
 }
--- Algorithms-And-DataStructures/TurboCollections/TurboCloneBST.cs
+++ Algorithms-And-DataStructures/TurboCollections/TurboCloneBST.cs
@@ -32,4 +32,54 @@ public class BST
         clone.right = CloneTree(node.right);
         return clone;
     }
+
+    public Node RemoveNode(int value)
+    {
+        return RemoveNode(root, value);
+    }
+
+    private Node RemoveNode(Node node, int value)
+    {
+        if (node == null)
+        {
+            return null;
+        }
+
+        if (value < node.data)
+        {
+            node.left = RemoveNode(node.left, value);
+        }
+        else if (value > node.data)
+        {
+            node.right = RemoveNode(node.right, value);
+        }
+        else
+        {
+            if (node.left == null)
+            {
+                return node.right;
+            }
+            else if (node.right == null)
+            {
+                return node.left;
+            }
+            else
+            {
+                node.data = MinValue(node.right);
+                node.right = RemoveNode(node.right, node.data);
+            }
+        }
+        return node;
+    }
+
+    private int MinValue(Node node)
+    {
+        int minValue = node.data;
+        while (node.left != null)
+        {
+            minValue = node.left.data;
+            node = node.left;
+        }
+        return minValue;
+    }
 }
