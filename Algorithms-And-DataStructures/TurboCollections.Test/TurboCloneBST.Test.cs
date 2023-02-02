76789cfab4520d382f7cb9a7b5cbc65ca21e55a3
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-02-02 11:19:04 +0100

    Update TurboCloneBST.Test.cs

--- Algorithms-And-DataStructures/TurboCollections.Test/TurboCloneBST.Test.cs
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboCloneBST.Test.cs
@@ -1,9 +1,31 @@
 ﻿using NUnit.Framework;
 
-namespace TurboCollections.Test;
-
 [TestFixture]
-public class TurboCloneBST_Test
+public class TurboCloneBSTTests
 {
-    
+    [Test]
+    public void CloneTreeWithValidBSTShouldReturnClonedBST()
+    {
+        BST bst = new BST();
+        bst.root = new Node(5);
+        bst.root.left = new Node(2);
+        bst.root.right = new Node(8);
+
+        BST clonedBST = new BST();
+        clonedBST.root = bst.CloneTree(bst.root);
+
+        Assert.AreEqual(bst.root.data, clonedBST.root.data);
+        Assert.AreEqual(bst.root.left.data, clonedBST.root.left.data);
+        Assert.AreEqual(bst.root.right.data, clonedBST.root.right.data);
+    }
+
+    [Test]
+    public void CloneTree_WithEmptyBST_ShouldReturnNull()
+    {
+        BST bst = new BST();
+        BST clonedBST = new BST();
+        clonedBST.root = bst.CloneTree(bst.root);
+
+        Assert.IsNull(clonedBST.root);
+    }
 }
